ebbc5a2c86bdd41071a3e645a8c64f28071f7c6f
Hudson Andrade Viana <hudson.viana@tributacao.pmp>
2024-01-23 12:02:10 -0300

    Código modificado para uso no frontend

--- index.js
+++ index.js
@@ -1,25 +1,104 @@
+// Global variables
 const btnValueInFull = document.getElementById('btn-value-in-full');
 const inputValue = document.getElementById('input-value');
 const response = document.getElementById('response');
 
-btnValueInFull.addEventListener('click', () => {
-  if (!inputValue.value || inputValue.value == 0) {
-    alert('Digite um valor para escrever por extenso.');
-    return;
+const units = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove'];
+const teens = ['onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove'];
+const tens = ['dez', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta', 'noventa'];
+const hundreds = ['cento', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos', 'setecentos', 'oitocentos', 'novecentos'];
+
+// Helpers
+const insertCharacterBetweenWords = (words, character) => {
+  return words.flatMap((word, index) => (index < words.length - 1 ? [word, character] : [word]));
+};
+
+const getWords = (value, arr) => {
+  const indexToDeduct = arr === teens ? 11 : 1;
+  return arr[value - indexToDeduct];
+};
+
+const convertNumberToWords = (numArray) => {
+  const words = [];
+  const digits = numArray.length;
+  const firstDigit = numArray[0];
+  const middleDigit = numArray[1];
+  const lastDigit = numArray[digits - 1];
+  const lastTwoDigits = numArray.slice(-2).join('');
+
+  if (digits === 3) {
+    if (numArray.join('') == 100) {
+      words.push('cem');
+    } else {
+      words.push(getWords(firstDigit, hundreds));
+      if (lastTwoDigits > 0 && lastTwoDigits < 10) {
+        words.push(getWords(lastDigit, units));
+      } else if (lastTwoDigits == 10) {
+        words.push(getWords(middleDigit, tens));
+      } else if (lastTwoDigits > 10 && lastTwoDigits <= 19) {
+        words.push(getWords(lastTwoDigits, teens));
+      } else if (lastTwoDigits > 19) {
+        words.push(getWords(middleDigit, tens));
+        if (lastDigit != 0) {
+          words.push(getWords(lastDigit, units));
+        }
+      }
+    }
   }
 
-  const inputValueSanitized = inputValue.value.replace(/\./g, '').replace(',', '.');
+  if (digits === 2) {
+    if (lastTwoDigits > 0 && lastTwoDigits < 10) {
+      words.push(getWords(lastDigit, units));
+    } else if (lastTwoDigits == 10) {
+      words.push(getWords(firstDigit, tens));
+    } else if (lastTwoDigits > 10 && lastTwoDigits <= 19) {
+      words.push(getWords(lastTwoDigits, teens));
+    } else if (lastTwoDigits > 19) {
+      words.push(getWords(firstDigit, tens));
+      if (lastDigit != 0) {
+        words.push(getWords(lastDigit, units));
+      }
+    }
+  }
 
-  const result = getValueInFull(+inputValueSanitized);
-  response.innerText = result;
-});
+  if (digits === 1) {
+    words.push(getWords(lastDigit, units));
+  }
 
-inputValue.addEventListener('keyup', (e) => (response.innerText = ''));
+  const wordsSanitized = words.filter(Boolean);
+  const resultWords = insertCharacterBetweenWords(wordsSanitized, 'e');
+  return resultWords;
+};
 
-const getValueInFull = (inputValue) => {
+const splitNumberIntoClasses = (inputNumber) => {
+  return inputNumber.match(/.{1,3}(?=(.{3})*$)/g);
+};
+
+const getClassName = (value, orderClass) => {
+  const classes = {
+    2: 'mil',
+    3: value == 1 ? 'milhão' : 'milhões',
+    4: value == 1 ? 'bilhão' : 'bilhões',
+    5: value == 1 ? 'trilhão' : 'trilhões',
+  };
+  return value != 0 ? classes[orderClass] : null;
+};
+
+const getClassSeparator = (className, remainder) => {
+  if ((remainder > 100 && remainder % 100 !== 0) || (remainder > 1000 && remainder % 1000 !== 0)) {
+    return ',';
+  } else if (className && remainder && (remainder <= 100 || remainder % 100 === 0)) {
+    return 'e';
+  } else if (className && className !== 'mil' && !remainder) {
+    return 'de';
+  }
+  return null;
+};
+
+const checkValidation = (inputValue) => {
   const validation = [
     {
-      test: typeof inputValue !== 'number',
+      test: isNaN(inputValue) || typeof inputValue !== 'number',
       msg: 'ERRO: O valor fornecido não é um número válido.',
     },
     {
@@ -29,10 +108,14 @@ const getValueInFull = (inputValue) => {
   ];
 
   const errorValidation = validation.find((check) => check.test);
+  return errorValidation?.msg;
+};
 
-  if (errorValidation) {
-    return errorValidation.msg;
-  }
+// Main function
+const getValueInFull = (inputValue) => {
+  // Validation
+  const errorValidation = checkValidation(inputValue);
+  if (errorValidation) return errorValidation;
 
   const result = [];
   const stringValue = String(inputValue.toFixed(2));
@@ -40,99 +123,8 @@ const getValueInFull = (inputValue) => {
   const currencyName = integerValue == 1 ? 'real' : 'reais';
   const fractionName = fractionValue == 1 ? 'centavo' : 'centavos';
 
-  const units = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove'];
-  const teens = ['onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove'];
-  const tens = ['dez', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta', 'noventa'];
-  const hundreds = ['cento', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos', 'setecentos', 'oitocentos', 'novecentos'];
-
-  const insertCharacterBetweenWords = (words, character) => {
-    return words.flatMap((word, index) => (index < words.length - 1 ? [word, character] : [word]));
-  };
-
-  const getWords = (value, arr) => {
-    const indexToDeduct = arr === teens ? 11 : 1;
-    return arr[value - indexToDeduct];
-  };
-
-  const convertNumberToWords = (numArray) => {
-    const words = [];
-    const digits = numArray.length;
-    const firstDigit = numArray[0];
-    const middleDigit = numArray[1];
-    const lastDigit = numArray[digits - 1];
-    const lastTwoDigits = numArray.slice(-2).join('');
-
-    if (digits === 3) {
-      if (numArray.join('') == 100) {
-        words.push('cem');
-      } else {
-        words.push(getWords(firstDigit, hundreds));
-        if (lastTwoDigits > 0 && lastTwoDigits < 10) {
-          words.push(getWords(lastDigit, units));
-        } else if (lastTwoDigits == 10) {
-          words.push(getWords(middleDigit, tens));
-        } else if (lastTwoDigits > 10 && lastTwoDigits <= 19) {
-          words.push(getWords(lastTwoDigits, teens));
-        } else if (lastTwoDigits > 19) {
-          words.push(getWords(middleDigit, tens));
-          if (lastDigit != 0) {
-            words.push(getWords(lastDigit, units));
-          }
-        }
-      }
-    }
-
-    if (digits === 2) {
-      if (lastTwoDigits > 0 && lastTwoDigits < 10) {
-        words.push(getWords(lastDigit, units));
-      } else if (lastTwoDigits == 10) {
-        words.push(getWords(firstDigit, tens));
-      } else if (lastTwoDigits > 10 && lastTwoDigits <= 19) {
-        words.push(getWords(lastTwoDigits, teens));
-      } else if (lastTwoDigits > 19) {
-        words.push(getWords(firstDigit, tens));
-        if (lastDigit != 0) {
-          words.push(getWords(lastDigit, units));
-        }
-      }
-    }
-
-    if (digits === 1) {
-      words.push(getWords(lastDigit, units));
-    }
-
-    const wordsSanitized = words.filter(Boolean);
-    const resultWords = insertCharacterBetweenWords(wordsSanitized, 'e');
-    return resultWords;
-  };
-
-  // GET INTEGER PART IN FULL
-
+  // Get integer value in full
   if (integerValue > 0) {
-    const splitNumberIntoClasses = (inputNumber) => {
-      return inputNumber.match(/.{1,3}(?=(.{3})*$)/g);
-    };
-
-    const getClassName = (value, orderClass) => {
-      const classes = {
-        2: 'mil',
-        3: value == 1 ? 'milhão' : 'milhões',
-        4: value == 1 ? 'bilhão' : 'bilhões',
-        5: value == 1 ? 'trilhão' : 'trilhões',
-      };
-      return value != 0 ? classes[orderClass] : null;
-    };
-
-    const insertClassSeparator = (className, remainder) => {
-      if ((remainder > 100 && remainder % 100 !== 0) || (remainder > 1000 && remainder % 1000 !== 0)) {
-        result.push(',');
-      } else if (className && remainder && (remainder <= 100 || remainder % 100 === 0)) {
-        result.push('e');
-      } else if (className && className !== 'mil' && !remainder) {
-        result.push('de');
-      }
-    };
-
     const integerSplitIntoClasses = splitNumberIntoClasses(integerValue);
     let orderClass = integerSplitIntoClasses.length;
 
@@ -150,15 +142,15 @@ const getValueInFull = (inputValue) => {
           .slice(i + 1)
           .filter((value) => value !== '000')
           .join('');
-        insertClassSeparator(className, remainder);
+        const classSeparator = getClassSeparator(className, remainder);
+        result.push(classSeparator);
       }
     }
 
     result.push(currencyName);
   }
 
-  // GET FRACTION PART IN FULL
-
+  // Get fraction value in full
   if (fractionValue > 0) {
     if (integerValue > 0) result.push('e');
     const charFractionArray = [...fractionValue];
@@ -168,6 +160,7 @@ const getValueInFull = (inputValue) => {
     result.push(fractionName);
   }
 
+  // Result
   const valueInFull = result
     .filter(Boolean)
     .join(' ')
@@ -175,3 +168,18 @@ const getValueInFull = (inputValue) => {
 
   return valueInFull;
 };
+
+// Listeners
+btnValueInFull.addEventListener('click', () => {
+  if (!inputValue.value || inputValue.value == 0) {
+    alert('Digite um valor para escrever por extenso.');
+    return;
+  }
+
+  const inputValueSanitized = inputValue.value.replace(/\./g, '').replace(',', '.');
+
+  const result = getValueInFull(+inputValueSanitized);
+  response.innerText = result;
+});
+
+inputValue.addEventListener('keyup', (e) => (response.innerText = ''));
